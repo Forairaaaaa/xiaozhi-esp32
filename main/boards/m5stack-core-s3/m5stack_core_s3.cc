5e7dca13c554cec280c74456c498cbaa55c82012
Forairaaaaa <applesyqd@outlook.com>
2024-12-24 11:56:12 +0800

    add aw9523 shit

--- main/boards/m5stack-core-s3/m5stack_core_s3.cc
+++ main/boards/m5stack-core-s3/m5stack_core_s3.cc
@@ -34,6 +34,75 @@ private:
         ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &i2c_bus_));
     }
 
+    void I2cDetect() {
+        uint8_t address;
+        printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
+        for (int i = 0; i < 128; i += 16) {
+            printf("%02x: ", i);
+            for (int j = 0; j < 16; j++) {
+                fflush(stdout);
+                address = i + j;
+                esp_err_t ret = i2c_master_probe(i2c_bus_, address, pdMS_TO_TICKS(200));
+                if (ret == ESP_OK) {
+                    printf("%02x ", address);
+                } else if (ret == ESP_ERR_TIMEOUT) {
+                    printf("UU ");
+                } else {
+                    printf("-- ");
+                }
+            }
+            printf("\r\n");
+        }
+    }
+
+    i2c_master_dev_handle_t aw9523_hendle_;
+    esp_err_t aw9523_write_reg_8(uint8_t reg, uint8_t data) {
+        uint8_t w_buf[2];
+        w_buf[0] = reg;
+        w_buf[1] = data;
+        return i2c_master_transmit(aw9523_hendle_, w_buf, 2, portMAX_DELAY);
+    }
+
+    void InitializeAw9523() {
+        const uint8_t AW9523_REG_CONFIG0  = 0x04;
+        const uint8_t AW9523_REG_CONFIG1  = 0x05;
+        const uint8_t AW9523_REG_GCR      = 0x11;
+        const uint8_t AW9523_REG_LEDMODE0 = 0x12;
+        const uint8_t AW9523_REG_LEDMODE1 = 0x13;
+
+
+        i2c_device_config_t aw9523_config = {};
+        aw9523_config.dev_addr_length = I2C_ADDR_BIT_LEN_7;
+        aw9523_config.device_address = 0x58;
+        aw9523_config.scl_speed_hz = 400000;
+        i2c_master_bus_add_device(i2c_bus_, &aw9523_config, &aw9523_hendle_);
+
+        aw9523_write_reg_8(AW9523_REG_LEDMODE0, 0xFF);
+        aw9523_write_reg_8(AW9523_REG_LEDMODE1, 0xFF);
+        aw9523_write_reg_8(AW9523_REG_CONFIG0, 0b00011000);
+        aw9523_write_reg_8(AW9523_REG_CONFIG1, 0b00001100);
+        aw9523_write_reg_8(AW9523_REG_GCR, (1 << 4));
+        // P0
+        aw9523_write_reg_8(0x02, 0b00000111);
+        // P1
+        aw9523_write_reg_8(0x03, 0b10000011);
+        
+        // uint8_t w_buf[2];
+        // uint8_t r_buf[1];
+        // w_buf[0] = 0x02;
+        // i2c_master_transmit_receive(aw9523_hendle, w_buf, 1, r_buf, 1, portMAX_DELAY);
+        // printf("read get: %d\n", r_buf[0]);
+
+        // w_buf[1] = r_buf[0] | 0b00000100;
+        // w_buf[0] = 0x02;
+        // i2c_master_transmit(aw9523_hendle, w_buf, 2, portMAX_DELAY);
+
+        while (1) {
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            I2cDetect();
+        }
+    }
+
     void InitializeButtons() {
         // boot_button_.OnClick([this]() {
         //     Application::GetInstance().ToggleChatState();
@@ -50,6 +119,8 @@ public:
     // M5StackCoreS3Board() : boot_button_(BOOT_BUTTON_GPIO) {
     M5StackCoreS3Board() {
         InitializeI2c();
+        InitializeAw9523();
+        I2cDetect();
         InitializeButtons();
         InitializeIot();
     }
