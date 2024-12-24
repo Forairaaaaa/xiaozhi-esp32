bd02706a61f8019882b4334808e18c8acc177a12
Forairaaaaa <applesyqd@outlook.com>
2024-12-24 11:04:11 +0800

    adding audio

--- main/boards/m5stack-core-s3/m5stack_core_s3.cc
+++ main/boards/m5stack-core-s3/m5stack_core_s3.cc
@@ -1,5 +1,5 @@
 #include "wifi_board.h"
-#include "audio_codecs/box_audio_codec.h"
+#include "audio_codecs/cores3_audio_codec.h"
 #include "display/no_display.h"
 #include "application.h"
 #include "button.h"
@@ -15,14 +15,14 @@
 class M5StackCoreS3Board : public WifiBoard {
 private:
     i2c_master_bus_handle_t i2c_bus_;
-    Button boot_button_;
+    // Button boot_button_;
 
     void InitializeI2c() {
         // Initialize I2C peripheral
         i2c_master_bus_config_t i2c_bus_cfg = {
             .i2c_port = (i2c_port_t)1,
-            .sda_io_num = AUDIO_CODEC_I2C_SDA_PIN,
-            .scl_io_num = AUDIO_CODEC_I2C_SCL_PIN,
+            .sda_io_num = I2C_SDA_PIN,
+            .scl_io_num = I2C_SCL_PIN,
             .clk_source = I2C_CLK_SRC_DEFAULT,
             .glitch_ignore_cnt = 7,
             .intr_priority = 0,
@@ -35,9 +35,9 @@ private:
     }
 
     void InitializeButtons() {
-        boot_button_.OnClick([this]() {
-            Application::GetInstance().ToggleChatState();
-        });
+        // boot_button_.OnClick([this]() {
+        //     Application::GetInstance().ToggleChatState();
+        // });
     }
 
     // 物联网初始化，添加对 AI 可见设备
@@ -47,7 +47,8 @@ private:
     }
 
 public:
-    M5StackCoreS3Board() : boot_button_(BOOT_BUTTON_GPIO) {
+    // M5StackCoreS3Board() : boot_button_(BOOT_BUTTON_GPIO) {
+    M5StackCoreS3Board() {
         InitializeI2c();
         InitializeButtons();
         InitializeIot();
@@ -59,11 +60,12 @@ public:
     }
 
     virtual AudioCodec* GetAudioCodec() override {
-        static BoxAudioCodec* audio_codec = nullptr;
+        // TODO
+        static CoreS3AudioCodec* audio_codec = nullptr;
         if (audio_codec == nullptr) {
-            audio_codec = new BoxAudioCodec(i2c_bus_, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            audio_codec = new CoreS3AudioCodec(i2c_bus_, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
                 AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
-                AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR, AUDIO_CODEC_ES7210_ADDR, AUDIO_INPUT_REFERENCE);
+                AUDIO_CODEC_AW88298_ADDR, AUDIO_CODEC_ES7210_ADDR, AUDIO_INPUT_REFERENCE);
             audio_codec->SetOutputVolume(AUDIO_DEFAULT_OUTPUT_VOLUME);
         }
         return audio_codec;
